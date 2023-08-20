d45f2c6634e76297ded6285572804d663191bbc8
박은지 <qkrdmswl1018@naver.com>
2023-08-20 11:22:51 +0900

    refactor: #4 `@JsonNaming`을 사용해 중복 코드 제거

--- src/main/java/dev/backlog/domain/auth/infrastructure/kakao/KakaoTokens.java
+++ src/main/java/dev/backlog/domain/auth/infrastructure/kakao/KakaoTokens.java
@@ -1,27 +1,22 @@
 package dev.backlog.domain.auth.infrastructure.kakao;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoTokens {
 
-    @JsonProperty("access_token")
     private String accessToken;
 
-    @JsonProperty("token_type")
     private String tokenType;
 
-    @JsonProperty("refresh_token")
     private String refreshToken;
 
-    @JsonProperty("expires_in")
     private int expiresIn;
 
-    @JsonProperty("refresh_token_expires_in")
     private int refreshTokenExpiresIn;
 }
