f3eeb28b65aecdfe0da8530fe19601365693a825
박은지 <qkrdmswl1018@naver.com>
2023-08-23 23:10:29 +0900

    refactor: #4 DTO에 `@Getter` 적용

--- src/main/java/dev/backlog/domain/auth/infrastructure/kakao/dto/KakaoLoginParams.java
+++ src/main/java/dev/backlog/domain/auth/infrastructure/kakao/dto/KakaoLoginParams.java
@@ -2,17 +2,15 @@ package dev.backlog.domain.auth.infrastructure.kakao.dto;
 
 import dev.backlog.domain.user.model.OAuthProvider;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class KakaoLoginParams {
 
     private String authorizationCode;
-
-    public String getAuthorizationCode() {
-        return authorizationCode;
-    }
 
     public OAuthProvider getOauthProvider() {
         return OAuthProvider.KAKAO;
