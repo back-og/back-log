cf04954b9e7b187ae80ab0dd83550e0846464d51
박은지 <qkrdmswl1018@naver.com>
2023-08-23 21:44:53 +0900

    test: #4 카카오 소셜 재로그인 테스트 코드 작성

--- src/test/java/dev/backlog/domain/auth/service/OAuthServiceTest.java
+++ src/test/java/dev/backlog/domain/auth/service/OAuthServiceTest.java
@@ -11,23 +11,28 @@ import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
 import dev.backlog.domain.user.model.Email;
 import dev.backlog.domain.user.model.OAuthProvider;
 import dev.backlog.domain.user.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class OAuthLoginServiceTest {
+class OAuthServiceTest {
 
     @Mock
     private UserRepository userRepository;
@@ -38,8 +43,19 @@ class OAuthLoginServiceTest {
     @Mock
     private RequestOAuthInfoService requestOAuthInfoService;
 
-    @InjectMocks
+    @Mock
+    private RestTemplate restTemplate;
+
     private OAuthLoginService oAuthLoginService;
+
+    private OAuthLogoutService oAuthLogoutService;
+
+    @BeforeEach
+    void setUp() {
+        oAuthLoginService = new OAuthLoginService(userRepository, authTokensGenerator, requestOAuthInfoService);
+        oAuthLogoutService = new OAuthLogoutService(restTemplate);
+        ReflectionTestUtils.setField(oAuthLogoutService, "apiUrl", "http://test-logout-api-url");
+    }
 
     @Test
     @DisplayName("카카오 소셜 회원가입에 성공한다.")
@@ -53,6 +69,7 @@ class OAuthLoginServiceTest {
         when(userRepository.findByEmail(any())).thenReturn(Optional.empty());
         when(userRepository.save(any())).thenReturn(user);
         when(authTokensGenerator.generate(any())).thenReturn(authTokens);
+
         AuthTokens result = oAuthLoginService.kakaoSignUp(params);
 
         assertThat(authTokens).isEqualTo(result);
@@ -61,16 +78,12 @@ class OAuthLoginServiceTest {
     @Test
     @DisplayName("카카오 소셜 회원가입 후 사용자는 로그인에 성공한다.")
     void kakaoLoginTest() {
+        /* 회원가입 */
         KakaoSignUpParams signUpParams = new KakaoSignUpParams("authorizationCode", "블로그제목제목");
         OAuthLoginAndSignUpParams oAuthSignUpParams = new OAuthLoginAndSignUpParams(signUpParams.getAuthorizationCode(), signUpParams.getOauthProvider());
         OAuthInfoResponse signUpResponse = new OAuthInfoResponse("닉네임", "프로필 사진", new Email("email123@gmail.com"), 123L, OAuthProvider.KAKAO);
         User user = new User("닉네임", new Email("email123@gmail.com"), "프로필 사진", "블로그 제목", 123L, OAuthProvider.KAKAO);
         AuthTokens authTokensAfterSignUp = AuthTokens.of("accessTokenForSignUp", "refreshTokenForSignUp", "Bearer", 3600L);
-
-        KakaoLoginParams loginParams = new KakaoLoginParams("authorizationCode");
-        OAuthLoginAndSignUpParams oAuthLoginParams = new OAuthLoginAndSignUpParams(loginParams.getAuthorizationCode(), loginParams.getOauthProvider());
-        OAuthInfoResponse loginResponse = new OAuthInfoResponse("닉네임", "프로필 사진", new Email("email123@gmail.com"), 123L, OAuthProvider.KAKAO);
-        AuthTokens authTokensAfterLogin = AuthTokens.of("accessTokenForLogin", "refreshTokenForLogin", "Bearer", 3600L);
 
         when(requestOAuthInfoService.request(eq(oAuthSignUpParams))).thenReturn(signUpResponse);
         when(userRepository.findByEmail(any())).thenReturn(Optional.empty());
@@ -81,6 +94,12 @@ class OAuthLoginServiceTest {
 
         assertThat(authTokensAfterSignUp).isEqualTo(resultOfSignUp);
 
+        /* 로그인 */
+        KakaoLoginParams loginParams = new KakaoLoginParams("authorizationCode");
+        OAuthLoginAndSignUpParams oAuthLoginParams = new OAuthLoginAndSignUpParams(loginParams.getAuthorizationCode(), loginParams.getOauthProvider());
+        OAuthInfoResponse loginResponse = new OAuthInfoResponse("닉네임", "프로필 사진", new Email("email123@gmail.com"), 123L, OAuthProvider.KAKAO);
+        AuthTokens authTokensAfterLogin = AuthTokens.of("accessTokenForLogin", "refreshTokenForLogin", "Bearer", 3600L);
+
         when(requestOAuthInfoService.request(eq(oAuthLoginParams))).thenReturn(loginResponse);
         when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
         when(authTokensGenerator.generate(any())).thenReturn(authTokensAfterLogin);
@@ -88,7 +107,6 @@ class OAuthLoginServiceTest {
         AuthTokens resultOfLogin = oAuthLoginService.kakaoLogin(loginParams);
 
         assertThat(authTokensAfterLogin).isEqualTo(resultOfLogin);
-
     }
 
     @Test
@@ -102,6 +120,52 @@ class OAuthLoginServiceTest {
 
         assertThatThrownBy(() -> oAuthLoginService.kakaoLogin(params))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("카카오 소셜 회원가입 된 사용자는 로그아웃 후 재로그인에 성공한다.")
+    void kakaoLogoutTest() {
+        /* 회원가입 */
+        KakaoSignUpParams signUpParams = new KakaoSignUpParams("authorizationCode", "블로그제목제목");
+        OAuthLoginAndSignUpParams oAuthSignUpParams = new OAuthLoginAndSignUpParams(signUpParams.getAuthorizationCode(), signUpParams.getOauthProvider());
+        OAuthInfoResponse signUpResponse = new OAuthInfoResponse("닉네임", "프로필 사진", new Email("email123@gmail.com"), 123L, OAuthProvider.KAKAO);
+        User user = new User("닉네임", new Email("email123@gmail.com"), "프로필 사진", "블로그 제목", 123L, OAuthProvider.KAKAO);
+        AuthTokens authTokensAfterSignUp = AuthTokens.of("accessTokenForSignUp", "refreshTokenForSignUp", "Bearer", 3600L);
+
+        when(requestOAuthInfoService.request(eq(oAuthSignUpParams))).thenReturn(signUpResponse);
+        when(userRepository.findByEmail(any())).thenReturn(Optional.empty());
+        when(userRepository.save(any())).thenReturn(user);
+        when(authTokensGenerator.generate(any())).thenReturn(authTokensAfterSignUp);
+
+        AuthTokens resultOfSignUp = oAuthLoginService.kakaoSignUp(signUpParams);
+
+        assertThat(authTokensAfterSignUp).isEqualTo(resultOfSignUp);
+
+        /* 로그인 */
+        KakaoLoginParams loginParams = new KakaoLoginParams("authorizationCode");
+        OAuthLoginAndSignUpParams oAuthLoginParams = new OAuthLoginAndSignUpParams(loginParams.getAuthorizationCode(), loginParams.getOauthProvider());
+        OAuthInfoResponse loginResponse = new OAuthInfoResponse("닉네임", "프로필 사진", new Email("email123@gmail.com"), 123L, OAuthProvider.KAKAO);
+        AuthTokens authTokensAfterLogin = AuthTokens.of("accessTokenForLogin", "refreshTokenForLogin", "Bearer", 3600L);
+
+        when(requestOAuthInfoService.request(eq(oAuthLoginParams))).thenReturn(loginResponse);
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
+        when(authTokensGenerator.generate(any())).thenReturn(authTokensAfterLogin);
+
+        AuthTokens resultOfLogin = oAuthLoginService.kakaoLogin(loginParams);
+
+        assertThat(authTokensAfterLogin).isEqualTo(resultOfLogin);
+
+        /* 로그아웃 */
+        when(restTemplate.postForEntity(anyString(), any(), eq(String.class)))
+                .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        oAuthLogoutService.kakaoLogout(authTokensAfterLogin.getAccessToken());
+
+        /* 재로그인 */
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
+
+        AuthTokens resultOfReLogin = oAuthLoginService.kakaoLogin(loginParams);
+
+        assertThat(resultOfReLogin).isNotNull();
     }
 
 }
