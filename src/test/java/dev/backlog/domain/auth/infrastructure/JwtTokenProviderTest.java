b692398021fc3954c8a88938ea86adaeda436245
박은지 <qkrdmswl1018@naver.com>
2023-08-24 17:00:11 +0900

    test: #4 JWT 토큰 생성 테스트 수정

--- src/test/java/dev/backlog/domain/auth/infrastructure/JwtTokenProviderTest.java
+++ src/test/java/dev/backlog/domain/auth/infrastructure/JwtTokenProviderTest.java
@@ -1,10 +1,13 @@
 package dev.backlog.domain.auth.infrastructure;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtTokenProviderTest {
@@ -13,15 +16,16 @@ class JwtTokenProviderTest {
     private JwtTokenProvider jwtTokenProvider;
 
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 1800000;
-    private static final String USER_ID = "1";
+    private static final Long USER_ID = 1L;
 
+    @DisplayName("JWT 토큰을 생성한다.")
     @Test
     void jwtTokenProvideTest() {
-        String accessToken = jwtTokenProvider.generate(USER_ID, new Date(new Date().getTime() + ACCESS_TOKEN_EXPIRE_TIME));
-
+        String accessToken = jwtTokenProvider.generate(USER_ID.toString(), new Date(new Date().getTime() + ACCESS_TOKEN_EXPIRE_TIME));
         Long userId = jwtTokenProvider.extractUserId(accessToken);
 
-        System.out.println("accessToken : " + accessToken);
-        System.out.println("userId : " + userId);
+        assertThat(accessToken).isNotNull();
+        assertThat(userId).isEqualTo(USER_ID);
     }
+
 }
