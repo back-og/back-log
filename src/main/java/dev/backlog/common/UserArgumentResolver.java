5a542f0f50969f8135a97c057073f698f3c0d8fc
박은지 <qkrdmswl1018@naver.com>
2023-09-05 21:28:52 +0900

    feat: #65 ArgumentResolver 구현

---
+++ src/main/java/dev/backlog/common/UserArgumentResolver.java
@@ -0,0 +1,41 @@
+package dev.backlog.common;
+
+import dev.backlog.domain.auth.model.oauth.JwtTokenProvider;
+import dev.backlog.domain.user.dto.UserPrincipal;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(UserPrincipal.class);
+    }
+
+    @Override
+    public UserPrincipal resolveArgument(MethodParameter parampeter, ModelAndViewContainer mavContainer,
+                                         NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) webRequest.getNativeRequest();
+        String authHeader = httpServletRequest.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            Long userId = jwtTokenProvider.extractUserId(token);
+
+            return new UserPrincipal(userId);
+        }
+        throw new IllegalArgumentException("잘못된 권한 헤더입니다.");
+    }
+
+}
+
--- src/main/java/dev/backlog/common/config/AuthWebConfig.java
+++ src/main/java/dev/backlog/common/config/AuthWebConfig.java
@@ -1,16 +1,30 @@
 package dev.backlog.common.config;
 
+import dev.backlog.common.UserArgumentResolver;
+import dev.backlog.domain.auth.model.oauth.JwtTokenProvider;
 import dev.backlog.domain.auth.model.oauth.converter.OAuthProviderConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class AuthWebConfig implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new OAuthProviderConverter());
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserArgumentResolver(jwtTokenProvider));
     }
 
 }
