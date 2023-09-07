4aa62f60227f9b7c91818dea26d1dbfc5a82a64a
yumyeonghan <mgtmh991013@gmail.com>
2023-09-07 15:02:21 +0900

    :sparkles: #74 @Getter 추가

--- src/main/java/dev/backlog/domain/user/exception/UserErrorCode.java
+++ src/main/java/dev/backlog/domain/user/exception/UserErrorCode.java
@@ -1,8 +1,10 @@
 package dev.backlog.domain.user.exception;
 
 import dev.backlog.common.exception.ErrorCode;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
     U004("유저를 찾을 수 없음"),
