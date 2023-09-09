ab6be5478a1a93c2ade0a1f195a9f5bf3611ecf3
jun <alap_u@naver.com>
2023-09-10 05:06:24 +0900

    :recycle: #80 Fixture생성 메서드를 분리

--- src/test/java/dev/backlog/common/fixture/DtoFixture.java
+++ src/test/java/dev/backlog/common/fixture/DtoFixture.java
@@ -4,6 +4,7 @@ import dev.backlog.domain.auth.AuthTokens;
 import dev.backlog.domain.auth.model.oauth.OAuthProvider;
 import dev.backlog.domain.auth.model.oauth.dto.SignupRequest;
 import dev.backlog.domain.post.dto.PostCreateRequest;
+import dev.backlog.domain.post.dto.PostUpdateRequest;
 
 import java.util.List;
 
@@ -36,6 +37,19 @@ public class DtoFixture {
                 true,
                 "썸네일",
                 "경로"
+        );
+    }
+
+    public static PostUpdateRequest 게시물수정요청() {
+        return new PostUpdateRequest(
+                null,
+                "변경된 제목",
+                "변경된 내용",
+                List.of("변경된 해쉬태그"),
+                "변경된 요약",
+                false,
+                "변경된 URL",
+                "변경된 경로"
         );
     }
 
