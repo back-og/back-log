be5cfe2476ddf3adcafd01ed9cb207ae3716eb2e
yumyeonghan <mgtmh991013@gmail.com>
2023-09-11 17:01:44 +0900

    :white_check_mark: #86 SeriesServiceTest 코드 작성

---
+++ src/test/java/dev/backlog/domain/series/service/SeriesServiceTest.java
@@ -0,0 +1,41 @@
+package dev.backlog.domain.series.service;
+
+import dev.backlog.common.fixture.DtoFixture;
+import dev.backlog.domain.series.dto.SeriesCreateRequest;
+import dev.backlog.domain.user.dto.AuthInfo;
+import dev.backlog.domain.user.infrastructure.persistence.UserJpaRepository;
+import dev.backlog.domain.user.model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static dev.backlog.common.fixture.EntityFixture.유저1;
+
+@SpringBootTest
+class SeriesServiceTest {
+
+    @Autowired
+    private SeriesService seriesService;
+
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @DisplayName("시리즈를 생성할 수 있다.")
+    @Test
+    void createTest() {
+        //given
+        User user = 유저1();
+        userJpaRepository.save(user);
+        AuthInfo authInfo = new AuthInfo(user.getId());
+        SeriesCreateRequest seriesCreateRequest = DtoFixture.시리즈생성요청();
+
+        //when
+        Long seriesId = seriesService.create(seriesCreateRequest, authInfo);
+
+        //then
+        Assertions.assertThat(seriesId).isNotNull();
+    }
+
+}
