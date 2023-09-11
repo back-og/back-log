182d06c0770280818d8d20900d7c9e652fd7da61
yumyeonghan <mgtmh991013@gmail.com>
2023-09-11 17:03:39 +0900

    :sparkles: #86 MethodArgumentNotValidException 대한 예외 처리 핸드러 작성

--- src/main/java/dev/backlog/common/advice/GlobalExceptionHandler.java
+++ src/main/java/dev/backlog/common/advice/GlobalExceptionHandler.java
@@ -1,19 +1,59 @@
 package dev.backlog.common.advice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.backlog.common.exception.GlobalErrorCode;
 import dev.backlog.common.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final ObjectMapper objectMapper;
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleException(NotFoundException exception) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exception) throws JsonProcessingException {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        logging(extractErrorMessage(fieldErrors));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), GlobalErrorCode.VALIDATION_ERROR));
+    }
+
+    private String extractErrorMessage(List<FieldError> fieldErrors) throws JsonProcessingException {
+        if (fieldErrors.size() == 1) {
+            return fieldErrors.get(0).getDefaultMessage();
+        }
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : fieldErrors) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return objectMapper.writeValueAsString(errors);
+    }
+
+    private void logging(String message) {
+        log.info(message);
     }
 
 }
