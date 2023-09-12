c9989f371ad989257a019daa2eebdcb055525f54
yumyeonghan <mgtmh991013@gmail.com>
2023-09-12 17:27:44 +0900

    :sparkles: #96 시리즈 수정 기능

--- src/main/java/dev/backlog/series/service/SeriesService.java
+++ src/main/java/dev/backlog/series/service/SeriesService.java
@@ -3,6 +3,7 @@ package dev.backlog.series.service;
 import dev.backlog.series.domain.Series;
 import dev.backlog.series.domain.repository.SeriesRepository;
 import dev.backlog.series.dto.SeriesCreateRequest;
+import dev.backlog.series.dto.SeriesUpdateRequest;
 import dev.backlog.user.domain.User;
 import dev.backlog.user.domain.repository.UserRepository;
 import dev.backlog.user.dto.AuthInfo;
@@ -22,6 +23,12 @@ public class SeriesService {
         User user = userRepository.getById(authInfo.userId());
         Series series = seriesCreateRequest.toEntity(user);
         return seriesRepository.save(series).getId();
+    }
+
+    public void updateSeries(AuthInfo authInfo, Long seriesId, SeriesUpdateRequest seriesUpdateRequest) {
+        User user = userRepository.getById(authInfo.userId());
+        Series series = seriesRepository.getByIdAndUser(seriesId, user);
+        series.updateName(seriesUpdateRequest.seriesName());
     }
 
 }
