1d85ce279fef8acd9e27e2ea38c4d558d22af7ae
jun <alap_u@naver.com>
2023-09-12 19:47:39 +0900

    :recycle: #94 예외 처리 리팩터링

--- src/main/java/dev/backlog/common/advice/GlobalExceptionHandler.java
+++ src/main/java/dev/backlog/common/advice/GlobalExceptionHandler.java
@@ -17,14 +17,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleException(NotFoundException exception) {
-        log.error("handleMethodArgumentNotValidException", exception);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ErrorResponse.of(exception.getErrorCode()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exception) {
-        log.error("handleMethodArgumentNotValidException", exception);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(GlobalErrorCode.VALIDATION_ERROR, exception.getBindingResult()));
     }
