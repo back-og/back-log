4f4b82d5daac2dbfa86da7f14681b8cdef3b8590
yumyeonghan <mgtmh991013@gmail.com>
2023-09-13 09:18:47 +0900

    :sparkles: #97 SeriesSummaryResponse 추가

---
+++ src/main/java/dev/backlog/series/dto/SeriesSummaryResponse.java
@@ -0,0 +1,26 @@
+package dev.backlog.series.dto;
+
+import dev.backlog.post.domain.Post;
+import dev.backlog.series.domain.Series;
+
+import java.time.LocalDateTime;
+
+public record SeriesSummaryResponse(
+        Long seriesId,
+        String thumbnailImage,
+        String name,
+        int postCount,
+        LocalDateTime updatedAt
+) {
+
+    public static SeriesSummaryResponse of(final Series series, final Post firstPost, final int postCount) {
+        return new SeriesSummaryResponse(
+                series.getId(),
+                firstPost.getThumbnailImage(),
+                series.getName(),
+                postCount,
+                series.getUpdatedAt()
+        );
+    }
+
+}
