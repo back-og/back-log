8ba4536bab99c0dd81062552a70163b7ada4c295
jun <alap_u@naver.com>
2023-09-14 03:23:20 +0900

    :recycle: #118 트랜잭션 어노테이션 작성

--- src/main/java/dev/backlog/like/service/PostLikeService.java
+++ src/main/java/dev/backlog/like/service/PostLikeService.java
@@ -10,10 +10,12 @@ import dev.backlog.user.domain.repository.UserRepository;
 import dev.backlog.user.dto.AuthInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PostLikeService {
 
