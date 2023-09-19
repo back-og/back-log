2a01a3716793c34be60a3830c81f12bede993aaa
박은지 <qkrdmswl1018@naver.com>
2023-09-20 00:56:09 +0900

    :recycle: #127 사용되지 않는 클래스 제거

--- src/main/java/dev/backlog/reply/infrastructure/persistence/ReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.backlog.reply.infrastructure.persistence;
-
-import dev.backlog.reply.domain.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
