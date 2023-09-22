daf023d0fedab9b7a335f90d52a923e1d500ab62
jun <alap_u@naver.com>
2023-09-22 18:56:08 +0900

    # 낙관적 락 로그 적용 및 해결 실패 테스트코드 작성

--- src/main/java/dev/backlog/like/service/PostLikeService.java
+++ src/main/java/dev/backlog/like/service/PostLikeService.java
@@ -9,11 +9,13 @@ import dev.backlog.user.domain.User;
 import dev.backlog.user.domain.repository.UserRepository;
 import dev.backlog.user.dto.AuthInfo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -24,12 +26,12 @@ public class PostLikeService {
     private final PostRepository postRepository;
 
     public LikeStatusResponse switchLike(Long postId, AuthInfo authInfo) {
+        log.info("switchLikeService");
         User user = userRepository.getById(authInfo.userId());
         Post post = postRepository.getById(postId);
         Optional<PostLike> postLike = postLikeRepository.findByUserAndPostId(user, postId);
 
         switchLike(postLike, post, user);
-
         boolean isLiked = postLike.isEmpty();
         return new LikeStatusResponse(post.getLikeCount(), isLiked);
     }
