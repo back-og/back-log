16531f84246175437c542570b24c86bf540c730c
yumyeonghan <mgtmh991013@gmail.com>
2023-09-22 17:56:00 +0900

    :sparkles: #132 advice 에 log 출력

--- src/main/java/dev/backlog/common/advice/GlobalExceptionHandler.java
+++ src/main/java/dev/backlog/common/advice/GlobalExceptionHandler.java
@@ -23,54 +23,63 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleException(RuntimeException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.of(GlobalErrorCode.INTERNAL_SERVER_ERROR));
     }
 
     @ExceptionHandler(DataLengthExceededException.class)
     public ResponseEntity<ErrorResponse> handleException(DataLengthExceededException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(ErrorResponse.of(exception.getErrorCode()));
     }
 
     @ExceptionHandler(InvalidAuthException.class)
     public ResponseEntity<ErrorResponse> handleException(InvalidAuthException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(ErrorResponse.of(exception.getErrorCode()));
     }
 
     @ExceptionHandler(MissMatchException.class)
     public ResponseEntity<ErrorResponse> handleException(MissMatchException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(exception.getErrorCode()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleException(NotFoundException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ErrorResponse.of(exception.getErrorCode()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(GlobalErrorCode.VALIDATION_ERROR, exception.getBindingResult()));
     }
 
     @ExceptionHandler(UnsatisfiedServletRequestParameterException.class)
     public ResponseEntity<ErrorResponse> handleException(UnsatisfiedServletRequestParameterException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(GlobalErrorCode.VALIDATION_ERROR));
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleException(MethodArgumentTypeMismatchException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(exception));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleException(HttpMessageNotReadableException exception) {
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(GlobalErrorCode.VALIDATION_ERROR));
     }
