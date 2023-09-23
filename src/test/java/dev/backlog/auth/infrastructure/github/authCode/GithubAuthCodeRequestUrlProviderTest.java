2797cda26baa327292e30af11c240db90ecac9c6
박은지 <qkrdmswl1018@naver.com>
2023-09-23 20:28:16 +0900

    :white_check_mark: #140 Github/KakaoAuthCodeRequestUrlProvider 테스트 코드 작성

---
+++ src/test/java/dev/backlog/auth/infrastructure/github/authCode/GithubAuthCodeRequestUrlProviderTest.java
@@ -0,0 +1,42 @@
+package dev.backlog.auth.infrastructure.github.authCode;
+
+import dev.backlog.auth.domain.oauth.OAuthProvider;
+import dev.backlog.auth.infrastructure.github.config.GithubProperties;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GithubAuthCodeRequestUrlProviderTest {
+
+    @Mock
+    private GithubProperties githubProperties;
+
+    @InjectMocks
+    private GithubAuthCodeRequestUrlProvider provider;
+
+    @Test
+    void oAuthProvider() {
+        OAuthProvider oAuthProvider = provider.oAuthProvider();
+
+        assertThat(oAuthProvider).isEqualTo(OAuthProvider.GITHUB);
+    }
+
+    @Test
+    void provide() {
+        String resultUrl = "https://github.com/login/oauth/authorize?client_id=testClientId&redirect_uri=https://testClientId/redirect";
+
+        when(githubProperties.getClientId()).thenReturn("testClientId");
+        when(githubProperties.getRedirectUrl()).thenReturn("https://testClientId/redirect");
+
+        String expectedUrl = provider.provide();
+
+        assertThat(resultUrl).isEqualTo(expectedUrl);
+    }
+
+}
