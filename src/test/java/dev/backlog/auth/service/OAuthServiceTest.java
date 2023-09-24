8229bf86eda965c8ec24844bfda1b0da0406065d
yumyeonghan <mgtmh991013@gmail.com>
2023-09-24 15:14:31 +0900

    :white_check_mark: #131 update auth test

--- src/test/java/dev/backlog/auth/service/OAuthServiceTest.java
+++ src/test/java/dev/backlog/auth/service/OAuthServiceTest.java
@@ -12,6 +12,8 @@ import dev.backlog.common.fixture.DtoFixture;
 import dev.backlog.common.fixture.EntityFixture;
 import dev.backlog.user.domain.User;
 import dev.backlog.user.domain.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +22,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static dev.backlog.common.fixture.DtoFixture.토큰생성;
+import static dev.backlog.common.fixture.EntityFixture.공개_게시물;
+import static dev.backlog.common.fixture.EntityFixture.댓글1;
+import static dev.backlog.common.fixture.EntityFixture.유저1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
@@ -45,6 +50,11 @@ class OAuthServiceTest {
 
     @Mock
     private JwtTokenProvider jwtTokenProvider;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
 
     @DisplayName("로그인 타입에 맞는 리다이렉트 할 url을 생성한다.")
     @Test
